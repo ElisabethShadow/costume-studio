7cbdbba76bede2bdaa1b742aa02c9e537a4e07b9
David Kloeren <d.kloeren@googlemail.com>
2021-05-26 02:58:29 +0200

    minor changes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,5 +7,11 @@ class UserController < ApplicationController
   def show
     @option = params[:option] || "bookings"
     @user = current_user
+
+    if params[:confirm_booking]
+      @booking_to_confirm = Booking.find(params[:confirm_booking].to_i)
+      @booking_to_confirm.confirmation = true
+      @booking_to_confirm.save
+    end
   end
 end
